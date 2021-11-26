8d03c5c69d297fc42f08ab1758a044b1ca53cffb
yangmingzhuo <yangmingzhuo@tetras.ai>
2021-11-26 20:21:20 +0800

    update

--- utils/mat_ssim.py
+++ utils/mat_ssim.py
@@ -18,14 +18,15 @@ def gen_mat(model, pretrain_model, dst_path, data_loader, batch_size, patch_size
         noisy, target = noisy.cuda(), target.cuda()
         with torch.no_grad():
             prediction = model(noisy)
-            prediction = prediction.data.cpu().numpy().astype(np.float32)
-            target = target.data.cpu().numpy().astype(np.float32)
+            prediction = prediction.data.cpu().permute(0, 2, 3, 1).numpy().astype(np.float32)
+            target = target.data.cpu().permute(0, 2, 3, 1).numpy().astype(np.float32)
             for i in range(prediction.shape[0]):
-                store_data_prediction[iteration * prediction.shape[0] + i, :, :, :] = prediction.permute(1, 2, 0)
-                store_data_clean[iteration * prediction.shape[0] + i, :, :, :] = target.permute(1, 2, 0)
+                store_data_prediction[iteration * prediction.shape[0] + i, :, :, :] = prediction[i]
+                store_data_clean[iteration * prediction.shape[0] + i, :, :, :] = target[i]
                 num += 1
 
     store_data_clean.resize((num, patch_size, patch_size, 3))
     store_data_prediction.resize((num, patch_size, patch_size, 3))
+    logger.info('Mat with best model and {} patches generated!'.format(num))
     sio.savemat(os.path.join(dst_path, 'denoised.mat'), {"denoised": store_data_prediction, })
     sio.savemat(os.path.join(dst_path, 'clean.mat'), {"clean": store_data_clean, })
