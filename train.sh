681a0e634876607f0eba8dcd1407f3b510c7a9ea
yangmingzhuo <yangmingzhuo@tetras.ai>
2021-12-08 12:29:19 +0800

    update

--- train.sh
+++ train.sh
@@ -35,11 +35,21 @@
 #    --exp_id=0 &
 
 #sidd
-srun -p aipe --gres=gpu:2 --job-name='DGNet' -w SH-IDC2-172-20-21-72 \
+#srun -p aipe --gres=gpu:2 --job-name='DGNet' -w SH-IDC2-172-20-21-72 \
+#nohup python train_multiDataset.py \
+#    --gpus=4,5 \
+#    --data_set1='renoir' \
+#    --data_set2='nind' \
+#    --data_set3='rid2021_v2' \
+#    --data_set_test='sidd' \
+#    --exp_id=0 &
+
+#renoir
+srun -p aipe --gres=gpu:2 --job-name='DGNet' -w SH-IDC2-172-20-21-216 \
 nohup python train_multiDataset.py \
     --gpus=4,5 \
-    --data_set1='renoir' \
-    --data_set2='nind' \
+    --data_set1='sidd' \
+    --data_set2='renoir' \
     --data_set3='rid2021_v2' \
-    --data_set_test='sidd' \
+    --data_set_test='nind' \
     --exp_id=0 &
--- validation.sh
+++ validation.sh
@@ -1,5 +1,5 @@
-srun -p aipe --gres=gpu:2 --job-name='DGNet' -w SH-IDC2-172-20-21-72 \
+srun -p aipe --gres=gpu:2 --job-name='DGNet' -w SH-IDC2-172-20-21-216 \
 nohup python validation.py \
-    --data_set='rid2021_v2' \
-    --pretrained='/mnt/lustre/yangmingzhuo/DGNet/logs_v2/model_ELU_UNet_gpu_0,1_ds_nind_ps_128_bs_32_ep_150_lr_0.0002_lr_min_1e-05_exp_id_0/checkpoint' \
+    --data_set='nind' \
+    --pretrained='/mnt/lustre/yangmingzhuo/DGNet/logs_v2/model_ELU_UNet_gpu_0,1_ds_rid2021_v2_ps_128_bs_32_ep_150_lr_0.0002_lr_min_1e-05_exp_id_0/checkpoint' \
     --gpus=6 &
