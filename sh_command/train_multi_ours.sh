02a3ea1dfd96720b405944a75900599befdeeb5d
yangmingzhuo <yangmingzhuo@tetras.ai>
2021-12-20 02:01:34 +0800

    update

--- sh_command/train_multi_ours.sh
+++ sh_command/train_multi_ours.sh
@@ -3,7 +3,7 @@
 #sidd
 srun -p aipe --gres=gpu:4 --job-name='DGNet' -w SH-IDC2-172-20-21-216 \
 nohup python -m torch.distributed.launch --nproc_per_node=4 --master_port=10316 train_multiDataset_ddp_ours_v2.py \
-    --gpus=1,2,3,4 \
+    --gpus=0,1,2,3 \
     --data_set1='renoir_v2' \
     --data_set2='nind' \
     --data_set3='rid2021_v2' \
