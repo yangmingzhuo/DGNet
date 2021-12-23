6e37d6d82419279b746f9dcd86adb94215f2437e
yangmingzhuo <yangmingzhuo@tetras.ai>
2021-12-23 13:25:52 +0800

    update

--- sh_command/train_multi_ours.sh
+++ sh_command/train_multi_ours.sh
@@ -16,17 +16,17 @@
 #    --temperature=20.0 \
 #    --exp_id='v3' &
 
-srun -p aipe --gres=gpu:4 --job-name='DGNet' -w SH-IDC2-172-20-21-216 \
+srun -p aipe --gres=gpu:4 --job-name='DGNet' -w SH-IDC2-172-20-21-72 \
 nohup python -m torch.distributed.launch --nproc_per_node=4 --master_port=10320 train_multiDataset_ddp_ours_model.py \
-    --gpus=4,5,6,7 \
+    --gpus=0,1,2,3 \
     --data_set1='renoir_v2' \
     --data_set2='nind' \
     --data_set3='rid2021_v2' \
     --data_set_test='sidd' \
     --batch_size=8 \
-    --lr_ad=1e-4 \
+    --lr_ad=2e-4 \
     --lr_min_ad=1e-5 \
-    --lambda_ad=0.1 \
+    --lambda_ad=0.001 \
     --lambda_kl=0.0 \
     --temperature=20.0 \
-    --exp_id='v1_no_sn' &
+    --exp_id='v1' &
